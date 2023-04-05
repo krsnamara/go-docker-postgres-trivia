b71bc3972e1174bce1716933cdfc286a90b43c79
krsnamara <martinj.fitzpatrick@gmail.com>
2023-04-04 22:01:50 -0400

    routes and handlers spun up

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,7 @@ func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hollow, World/")
-	})
+	setupRoutes(app)
 
 	app.Listen(":3000")
 }
